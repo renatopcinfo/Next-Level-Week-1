9539f9a285787d7b8627b600155cc8fcc5bf6459
renatopcinfo <renatopcinfo@gmail.com>
2020-06-21 19:51:25 -0300

    items

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -110,7 +110,7 @@ const CreatePoint: React.FC = () => {
 
           <ul className="items-grid">
             {items.map(item => (
-              <li>
+              <li key={item.id}>
                 <img src={item.image_url} alt={item.title} />
                 <span>{item.title}</span>
               </li>
